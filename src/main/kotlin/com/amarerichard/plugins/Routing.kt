7fbf56c0f3269fea371aa9d151fdaf686927efb6
Mathiasabor <ahomagi0@gmail.com>
2024-02-05 14:04:06 +0100

    second commer123588sdfnzd

--- src/main/kotlin/com/amarerichard/plugins/Routing.kt
+++ src/main/kotlin/com/amarerichard/plugins/Routing.kt
@@ -3,10 +3,13 @@ package com.amarerichard.plugins
 import com.amarerichard.routes.Projectroutes
 import com.amarerichard.routes.Ticketlistroutes
 import com.amarerichard.routes.Ticketroutes
+import com.amarerichard.services.Projectservice
 import com.amarerichard.utils.RabbitMQFactory
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 fun Application.configureRouting() {
 
@@ -17,7 +20,14 @@ fun Application.configureRouting() {
         Ticketlistroutes()
         Ticketroutes()
         RabbitMQFactory().defaultChannelAndQueue().listeningMessage()
+       get("/") {
+           val projectservice = Projectservice()
+           val list = projectservice.getAllProject()
+           val jsonString = Json.encodeToString(list)
+           RabbitMQFactory().sendMessage(jsonString.toByteArray())
 
+           call.respond(projectservice.getAllProject())
+       }
 
 
     }
